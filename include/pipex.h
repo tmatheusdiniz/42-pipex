670bf3293c219512d48689d4e510ba96c870cf5e
Matheus Diniz <mreinald@student.42porto.com>
2025-02-10 22:56:16 +0000

    creating pipex.h and adding a few things such that two basics struct for start the development, i will after add many error messages for return for each error type

---
+++ include/pipex.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreinald <mreinald@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 14:37:39 by mreinald          #+#    #+#             */
+/*   Updated: 2025/02/10 14:58:21 by mreinald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/Libft/include/libft.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct s_cmd
+{
+	char	*path;
+	pid_t	*pid;
+}	t_cmd;
+
+typedef struct s_pipex
+{
+	int**pipe;
+	int	fd_in;
+	int	fd_out;
+	int	number_commands;
+	int	here_doc;
+	int	limiter;
+}	t_pipex;
+
+// Core
+int	main(int v, char **str, char **env_var);
+
+// Parse
+int	find_bin(t_cmd **cmd, char *command, char **env_var);
+int	open_files(t_pipex **pipex, char *infile, char *outfile);
+
+// Cleanup
+void	handle_errors(t_cmd **cmd, t_pipex **pipex, int index);
