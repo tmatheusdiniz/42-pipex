4e9155f0a3a48ee767c201b5b0e9a9037d5d4583
Matheus Diniz <mreinald@student.42porto.com>
2025-02-12 20:51:31 +0000

    fixing bugs: initializing the struct's members correctly and initializing everything before starting

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -14,7 +14,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-static void	init_all(t_cmd **cmd, t_pipex **pipex, int count_arg);
+static void	init_all(t_cmd **cmd, t_pipex **pipex, char **str, int count_arg);
 static int	create_pipe(t_pipex **pipex);
 
 static void	pipe_cleanup(t_pipex **pipex, int index)
@@ -44,12 +44,17 @@ static int	create_pipe(t_pipex **pipex)
 	return (0);
 }
 
-static void	init_all(t_cmd **cmd, t_pipex **pipex, int count_arg)
+static void	init_all(t_cmd **cmd, t_pipex **pipex, char **str, int count_arg)
 {
 	*cmd = (t_cmd *)malloc(sizeof(t_cmd));
 	*pipex = (t_pipex *)malloc(sizeof(t_pipex));
 	if (!*cmd || !*pipex)
 		handle_errors(cmd, pipex, 1);
+	(*cmd)->path = NULL;
+    (*cmd)->commands = NULL;
+    (*cmd)->pid = NULL;
+	(*pipex)->fd_in = 0;
+	(*pipex)->fd_out = 0;
 	(*pipex)->number_commands = count_arg - 3;
 	(*pipex)->pipe = (int **)malloc(sizeof(int *)
 			* ((*pipex)->number_commands + 1));
@@ -60,14 +65,24 @@ static void	init_all(t_cmd **cmd, t_pipex **pipex, int count_arg)
 	(*cmd)->pid = (pid_t *)malloc(sizeof(pid_t) * (*pipex)->number_commands);
 	if (!(*cmd)->pid)
 		handle_errors(cmd, pipex, 1);
+	(*cmd)->infile = str[1];
+	(*cmd)->outfile = str[count_arg - 1];
 }
 
 int	main(int count_arg, char **str, char **env_var)
 {
 	t_cmd	*cmd;
 	t_pipex	*pipex;
-	init_all(&cmd, &pipex, count_arg);	
-	if (open_files(&pipex, str[1], str[count_arg - 1]) == 4)
+
+	if (count_arg < 5)
+	{
+		ft_printf("You should send least 5 paremeters!\n");
+		exit (0);
+	}
+	init_all(&cmd, &pipex, str, count_arg);
+	if (open_files(&pipex, cmd->infile, cmd->outfile) == 4)
 		handle_errors(&cmd, &pipex, 4);
 	handle_forks(str, &cmd, &pipex, env_var);
+	clean_cmd(&cmd);
+	clean_pipex(&pipex);
 }
